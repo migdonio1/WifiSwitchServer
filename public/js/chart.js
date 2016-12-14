430b5097dcebca4fd2ffbdb340b035a475d5da93
MiguelBurgos <miguelburgosric@hotmail.com>
2016-12-14 02:56:59 -0600

    Script para gráficas añadido

---
+++ public/js/chart.js
@@ -0,0 +1,29 @@
+$(function (info) {
+
+    var data = [];
+
+    for(var i = 0; i < info.length; i++){
+        data.push([new Date(info[i].date).getTime(), info[i].value]);
+    }
+    // Create the chart
+
+    Highcharts.stockChart('container', {
+
+
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'AAPL Stock Price'
+        },
+
+        series: [{
+            name: 'AAPL',
+            data: data,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    });
+});
